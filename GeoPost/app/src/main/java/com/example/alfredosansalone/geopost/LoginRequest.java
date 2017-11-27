740de6fac90825230a1fe91a5c99228aba651092
mace6803 <alfredo@sansalone.net>
2017-11-27 17:16:17 +0100

    aggiornato il login e il loginrequest

--- GeoPost/app/src/main/java/com/example/alfredosansalone/geopost/LoginRequest.java
+++ GeoPost/app/src/main/java/com/example/alfredosansalone/geopost/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.example.alfredosansalone.geopost;
 
 import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
 import java.util.HashMap;
@@ -10,10 +11,10 @@ import java.util.Map;
  * Created by alfredosansalone on 20/11/17.
  */
 
-/* public class LoginRequest extends StringRequest {
+public class LoginRequest extends StringRequest {
     private Map<String, String> mParams;
 
-    public LoginRequest(String username, String password, String url) {
+    public LoginRequest(String username, String password, String url, Response.Listener<String> listener, Response.ErrorListener errorListener ) {
         super(Request.Method.POST, url, listener, errorListener);
         mParams = new HashMap<String, String>();
         mParams.put("username", username);
@@ -24,4 +25,4 @@ import java.util.Map;
     public Map<String, String> getParams(){
         return mParams;
     }
-} */
+}
--- GeoPost/app/src/main/java/com/example/alfredosansalone/geopost/intent/Login.java
+++ GeoPost/app/src/main/java/com/example/alfredosansalone/geopost/intent/Login.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,22 +14,47 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 //import com.example.alfredosansalone.geopost.LoginRequest;
+import com.example.alfredosansalone.geopost.LoginRequest;
 import com.example.alfredosansalone.geopost.R;
 
 public class Login extends AppCompatActivity {
 
-    RequestQueue queue;
     String risp;
+    //MySingleton id_se;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText password = (EditText)findViewById(R.id.password);
+        String user = username.getText().toString();
+        String passw = password.getText().toString();
 
-        queue = Volley.newRequestQueue(this);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://ewserver.di.unimi.it/mobicomp/geopost/login";
 
         //queue.add(LoginRequest);
+        LoginRequest loginRequest= new LoginRequest(user, passw, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        risp = response;
+                        // Display the first 500 characters of the response string.
+                        Log.d("Login", "Response is: "+ risp);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                risp = "That didn't work!";
+                Log.d("Login", "That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(loginRequest);
     }
+
 
     public void Login(View v){
         Log.d("Sono nella MainActivity", "myTap");
