a9da5a0c3b4ba04cd5b826ac493ac6c506424d48
mace6803 <alfredo@sansalone.net>
2017-12-13 16:09:33 +0100

    posizionamento centrale d

--- GeoPost/app/src/main/java/com/example/alfredosansalone/geopost/intent/AmiciSeguiti.java
+++ GeoPost/app/src/main/java/com/example/alfredosansalone/geopost/intent/AmiciSeguiti.java
@@ -8,12 +8,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.alfredosansalone.geopost.R;
 import com.example.alfredosansalone.geopost.fragment.ListFragment;
 import com.example.alfredosansalone.geopost.fragment.MapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AmiciSeguiti extends AppCompatActivity {
     String idsession;
+    RequestQueue queue;
 
     //PULSANTE GESTIONE VISTA
     public void Vista(View v){
@@ -43,12 +56,51 @@ public class AmiciSeguiti extends AppCompatActivity {
         transaction.replace(R.id.content, new MapFragment());
         transaction.commit();
 
+        queue = Volley.newRequestQueue(this);
+
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         idsession = MyModel.getInstance().getIdsession();
+
+        String url = "https://ewserver.di.unimi.it/mobicomp/geopost/followed?session_id=" + idsession;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("Amiciseguiti", "response is " + response);
+
+                        try {
+                            JSONObject risp = new JSONObject(response);
+                            Log.d("Amiciseguiti", risp.toString());
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                //Gestione errori
+                Log.d("Amiciseguiti", "on error response is " + volleyError);
+                if(volleyError.networkResponse != null && volleyError.networkResponse.data != null) {
+                    VolleyError error = new VolleyError(new String(volleyError.networkResponse.data));
+                    volleyError = error;
+                    Log.d("Amiciseguiti", "volleyError is " + volleyError);
+                    String errore = volleyError.toString().replace("com.android.volley.VolleyError: ", "");
+                    Log.d("Amiciseguiti", "stringa di errore " + errore);
+                }
+            }
+        });
+        queue.add(stringRequest);
     }
 
     // PULSANTE MENU A SCOMPARSA
@@ -69,19 +121,18 @@ public class AmiciSeguiti extends AppCompatActivity {
 
     // INTENT PER APRIRE NUOVE ACTIVITY
     public void Stato(View v) {
-        Log.d("Sono nella MainActivity", "myTap");
         Intent intent = new Intent(this, AggStato.class);
         startActivity(intent);
     }
 
     public void Profilo(View v) {
-        Log.d("Sono nella MainActivity", "myTap");
+
         Intent intent = new Intent(this, Profilo.class);
         startActivity(intent);
     }
 
     public void AggAmici(View v) {
-        Log.d("Sono nella MainActivity", "myTap");
+
         Intent intent = new Intent(this, AggAmici.class);
         startActivity(intent);
     }
